17aaf35f481dd87de07a2faaa42d689fb9006a43
Siddhanth Shah <siddhanth.shah@somaiya.edu>
2023-02-22 22:50:10 +0530

    added media root in settings.py

--- img_up/settings.py
+++ img_up/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# added by self
+MEDIA_ROOT=BASE_DIR/'media'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
