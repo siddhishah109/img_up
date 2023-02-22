dd1a69cb85b14f11c6b78fb532e55dfeb34cf37a
Siddhanth Shah <siddhanth.shah@somaiya.edu>
2023-02-22 22:46:33 +0530

    made model, resister it and added to urls

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import image
 # Register your models here.
+@admin.register(image)
+class imageAdmin(admin.ModelAdmin):
+    list_display=['id','photo','date']
