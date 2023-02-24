d3013dbad10636de591a4ee896c4f0b5b8bb4bb1
Siddhi Shah <shahsiddhi777@gmail.com>
2023-02-24 20:15:02 +0530

    change in views , image submit

--- myapp/views.py
+++ myapp/views.py
@@ -4,5 +4,9 @@ from .models import image
 
 # Create your views here.
 def home(request):
+    if request.method =='POST':
+        form =imageform(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
     form= imageform()
     return render(request,'home.html',{'form':form})
